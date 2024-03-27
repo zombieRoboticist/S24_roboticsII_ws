ee6abbc842688500f3fd7f58dc8d6563bd4c55b2
Cailean Sorce <caileansorce@gmail.com>
2024-03-26 20:04:47 -0400

    asf

--- src/tracking_control/tracking_control/tracking_node.py
+++ src/tracking_control/tracking_control/tracking_node.py
@@ -162,8 +162,8 @@ class TrackingNode(Node):
         # TODO: Update the control velocity command
         cmd_vel = Twist()
         cmd_vel.linear.x = kx * (current_object_pose[0] - x_offset)
-        cmd_vel.linear.y = 0
-        cmd_vel.angular.z = ka * current_object_pose[2]
+        cmd_vel.linear.y = 0.0
+        cmd_vel.angular.z = ka * current_object_pose[1]
         return cmd_vel
     
         ############################################
